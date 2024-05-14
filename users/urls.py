be30e19f8abfb48ba19a4ee67be87d994d170616
Zaone999 <waelbougarfa@gmail.com>
2024-05-14 20:39:37 +0200

    fix typo

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from .views import Create_Profile
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
-    path('logout/,', auth_views.LogoutView.as_view(template_name = 'users/logged_out.html'), name='logout'), 
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logged_out.html'), name='logout'), 
     path('signup/', Create_Profile.as_view(), name='signup') # Ensure this line is correct
 ]
